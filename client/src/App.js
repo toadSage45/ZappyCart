f924d5f05aeb98f30a009cfeb688ff58c01326b7
abhiyadav-2307 <toadSage45>
2025-04-03 18:13:21 +0530

    Complete Registration

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/ReactToastify.css';
-
+import { createStore } from 'redux'
 
 
 
@@ -14,6 +14,9 @@ import Login from "./pages/auth/Login.js";
 import Register from "./pages/auth/Register.js";
 import Home from "./pages/Home.js";
 import Header from "./components/nav/Header.js";
+import RegisterComplete from "./pages/auth/RegisterComplete.js";
+
+
 function App() {
   return (<>
     <Header/>
@@ -22,6 +25,7 @@ function App() {
       <Route exact path="/" element={<Home />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/register" element={<Register />} />
+      <Route exact path="/register/complete" element={<RegisterComplete />} />
     </Routes>
     </>
   );
