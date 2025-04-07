b4a37e4d35b61a4fe57e75978858202cfb4f81d3
toadSage45 <alok.20223026@mnnit.ac.in>
2025-04-07 19:34:36 +0530

    add createOrUpdateUser, return user in response, and implement /current-user endpoint

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,40 @@
-export const createOrUpdateUser = (req , res) => {
-    res.json({
-        data : "chod diya bhai",
-    })
+import User from '../models/user.js'
+
+
+
+
+export const createOrUpdateUser = async (req , res) => {
+    const {name , picture  , email } = req.user;
+
+    const user = await User.findOneAndUpdate({email : email} , {name : email.split('@')[0] , picture} , {new : true})
+
+
+    if(user){
+        console.log('User Updated' , user);
+        res.json(user);
+    }else{
+        const newUser = await new User({
+            email , 
+            name : email.split('@')[0], 
+            picture, 
+        }).save();
+
+        console.log('New user created' , newUser);
+        res.json(newUser);
+        
+    }
+
+    
+}
+
+export const currentUser = async (req  , res) => {
+
+    try {
+        const user = await User.findOne({email: req.user.email})
+        res.json(user);
+
+    } catch (error) {
+        console.error("Error in currentUser:", err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
 }
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,19 @@
 import admin from '../firebase/index.js'
 
-export const authCheck = (req , res , next ) => {
-    console.log(req.headers);
-    next();
+export const authCheck = async (req , res , next ) => {
+    //console.log(req.headers);
+    try {
+        const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken);
+
+        //console.log('Firebase user in authcheck' , firebaseUser);
+        req.user = firebaseUser ;
+        next();
+
+     } catch (error) {
+        
+       res.status(401)
+          .json({
+            error : "Invalid or expired token",
+          })
+    }
 }
