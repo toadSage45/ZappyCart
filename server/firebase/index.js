1ea800ac4d311b92299cafe5668623f4ea3791bc
akshat-kedia1 <akshat.kedia2021@gmail.com>
2025-07-15 15:36:53 +0530

    Preparing to deploy

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,12 +1,19 @@
-import admin from 'firebase-admin'
+import admin from 'firebase-admin';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-const serviceAccount = require("../config/fbServiceAccountKey.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf-8')
+  );
+} else {
+  serviceAccount = require('../config/fbServiceAccountKey.json');
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
-
 
 export default admin;
